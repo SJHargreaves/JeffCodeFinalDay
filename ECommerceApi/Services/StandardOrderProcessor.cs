cb9caf410f2e9947148ff87a8de3056431f98e6f
SJHargreaves <87990644+SJHargreaves@users.noreply.github.com>
2021-10-27 14:34:36 -0400

    Initial commit

---
+++ ECommerceApi/Services/StandardOrderProcessor.cs
@@ -0,0 +1,16 @@
+﻿using ECommerceApi.Models.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Services
+{
+    public class StandardOrderProcessor : IProcessOrders
+    {
+        public async Task<OrderResponse> ProcessOrderAsync(OrderPostRequest request)
+        {
+            return new OrderResponse();
+        }
+    }
+}
